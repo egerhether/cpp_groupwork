8fb8156ed13682536644ea46508c77ae8b79ce7a
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-09 14:20:36 +0000

    Add files via upload

--- Petros/HW1/4/RSL.cc
+++ Petros/HW1/4/RSL.cc
@@ -4,12 +4,10 @@ namespace {				//anonymous namespace
 char const myString[]=			//Defining the RSL
 					//We use delimiter as a marker to
 					//deal with "( and )" in the str
-R"delimiter(				
-^\s+Encryption key:(\w+)
+R"delimiter(^\s+Encryption key:(\w+)
 ^\s+Quality=(\d+)
 ^\s+E?SSID:"([[:print:]]+)"
 ^\s+ssid="([[:print:]]+)"
-
 )delimiter";
 };	
 	
