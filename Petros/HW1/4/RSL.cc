abdd7dcdee9c89adb52d5a646806c86206807b39
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-09 14:39:51 +0200

    Add files via upload

--- Petros/HW1/4/RSL.cc
+++ Petros/HW1/4/RSL.cc
@@ -1,9 +1,34 @@
 #include<iostream>
 
+namespace {				//anonymous namespace
+char const encryptionKey[]=		//Defining the RSL
+R"(
+^\s+Encryption key:(\w+)
+)";
+
+char const quality[]=
+R"(
+^\s+Quality=(\d+)
+)";
+
+char const ESSID[]=			//Here we define the RSL
+					//between delimiter markers as to not
+					//have problems with "( and )"
+R"delimiter(
+^\s+E?SSID:"([[:print:]]+)"
+)delimiter";
+
+char const SSID[]=
+R"delimiter(
+^\s+ssid="([[:print:]]+)"
+)delimiter";
+};	
+	
 int main()
 {
-	std::cout << "^\\s+Encryption key:(\\w+)"	<< '\n' <<
-		     "^\\s+Quality=(\\d+)"		<< '\n' <<
-		     "^\\s+E?SSID:\"([[:print:]]+)\""   << '\n' <<
-		     "^\\s+ssid=\"([[:print:]]+)\""	<< '\n';
-}	
+	std::cout << encryptionKey << '\n' <<
+		     quality	   << '\n' <<
+     		     ESSID         << '\n' <<
+		     SSID	   << '\n';
+		     		     
+}
