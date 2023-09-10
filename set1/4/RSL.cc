5c2bc723e48f277d3e5fd524c8f5c9a7bf436035
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-10 15:15:57 +0000

    Add files via upload

---
+++ set1/4/RSL.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+namespace {				//anonymous namespace
+char const myString[]=			//Defining the RSL
+					//We use delimiter as a marker to
+					//deal with "( and )" in the str
+R"delimiter(^\s+Encryption key:(\w+)
+^\s+Quality=(\d+)
+^\s+E?SSID:"([[:print:]]+)"
+^\s+ssid="([[:print:]]+)"
+)delimiter";
+};	
+	
+int main()
+{
+	std::cout << myString;
+		     		     
+}
