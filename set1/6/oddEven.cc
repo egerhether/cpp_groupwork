a3fcefb9c93849091ab0e3af9a013e1c7b795f8c
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-11 14:12:28 +0100

    Update oddEven.cc

--- set1/6/oddEven.cc
+++ set1/6/oddEven.cc
@@ -1,52 +1,52 @@
-#include<iostream>
+# include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int value;
-	cout << "input an integer:\n";
-	cin >> value;			//we stream int into "value"
-	
-					//if division by 2 with remainder
-					//is == 1 then value is odd
-	cout << ((value % 2 == 1) ? "odd" : "even") << '\n';
-	
-					//As value is an int, dividing it by
-					//2 truncates any fractional part,
-					//acting as a division and floor fct
-					//so if value is odd, this expression
-					//will not be equal to value.
-	cout << (((value / 2) + (value / 2)) == value 
-		? "even" : "odd") << '\n';
-					
-					//we use the bitwise or operator
-					//to compare our value to 1
-					//(1 = 00b00...001 so it can be
-					//compared bitwise to value)
-					//meaning that all the digits
-					//other than the last one will
-					//remain the same, as 
-					//they are compared to 0, 
-					//while the last one will remain
-					//the same if it is 1 or get
-					//incremented by 1 if it is 0
-					//(we compare it to 1) therefore
-					//if value is odd its last digit 
-					//is = 1 so value | 1 == value
-					//otherwise value | 1 == value + 1
-	cout << ((value | 1) == value ? "odd" : "even") << '\n';
+    size_t value;
 
-					//we compare using the bitwise
-					//and operator  our value and 1
-					//(comparing all digits with 0 
-					//except the last one which we
-					//compare with 1)
-					//meaning that the output
-					//will be true (1) if
-					//the last digit of our value is one
-					//(in bin representation) iff
-					//value is odd.
-	cout << (value & 1 ? "odd" : "even") << '\n';
+    cin >> value;
+                    // simple modulo test, if remainder from diving by 2 is 1 
+                    // it is odd
+    cout << ((value % 2 == 1) ? "odd" : "even") << '\n';
+    
+                    // the bit form of 1 is "0001" and all odd numbers end
+                    // with 1 in their bit form, meaning that the last bit
+                    // of "odd & 1" is always 1 while the rest of the bits
+                    // are 0 due to "0 & 1 == 0" making "value & 1 == 1" for 
+                    // all odd numbers. The reverse is true for evens as their
+                    // last bit is 0 and all the previous bits are made to be
+                    // 0 same as for odds.
+    cout << (((value & 1) == 1) ? "odd" : "even") << '\n';
+    
+                    // due to value being a size_t type "value / 2" will also
+                    // be size_t type meaning that if value is odd then 
+                    // "value / 2" will result in only the whole part of the 
+                    // divison as size_t cannot be a fraction. The fractional part
+                    // gets truncated. this means that only even numbers
+                    // are equal to themselves divided then multiplied by 2.0
+    cout << ((value / 2 * 2 == value) ? "even" : "odd") << '\n';
+    
+                    // odd numbers have a 1 at the end of their bit form and
+                    // by using the non-exclusive or operator this one is
+                    // is preserved when "odd | 1" similar to the rest of 
+                    // the numbers bit form. For evens this is not true as
+                    // they have 0 at the end meaning "even | 1 == even + 1".
+    cout << (((value | 1) == value) ? "odd" : "even") << '\n';
+    
+                    // as odd numbers have 1 at their last bit and so does 1
+                    // itself then the ^ operator sees that the last bit is
+                    // both 1 making "odd ^ 1" have 0 at the end instead of 1,
+                    // making it smaller by one. For even numbers, they have 
+                    // 0 at the end making "even ^ 1" be larger by 1
+    cout << (((value ^ 1) == (value - 1)) ? "odd" : "even") << '\n';
+    
+                    // as odd numbers have a 1 at the end in their bit form
+                    // then shifting them by a bit right and back left removes 
+                    // the 1 at the end changing it to 0 meaning they're not
+                    // equal anymore. since even numbers have 0 at the end
+                    // this procedure doesn't change their value
+    cout << (((value >> 1) << 1 == value) ? "even" : "odd") << '\n';
 
-	}	
+}
