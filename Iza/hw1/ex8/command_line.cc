31100ce944dd7f0d0110395cc4231f9520445381
Izabela Kurek <ikurek0309@gmail.com>
2023-09-11 14:26:38 +0200

    corrections

--- Iza/hw1/ex8/command_line.cc
+++ Iza/hw1/ex8/command_line.cc
@@ -1,10 +1,11 @@
 # include <string>
 # include <iostream>
 
+using namespace std;
+
 int main(int argc, char* argv[])
 {
-    std::cout << "hello " << ((argc == 1) ? "world" : 
-        ((argc == 2) ? argv[1] : argv[std::stoul(argv[1])])) << '\n';
-
+    cout << "hello " << ((argc == 1) ? "world" : 
+        ((argc == 2) ? argv[1] : argv[stoul(argv[1])])) << '\n';
 }
 
