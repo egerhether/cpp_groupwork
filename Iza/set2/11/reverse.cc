ac6cbb503d31196f586eaad563df80e8dd205e7e
Izabela Kurek <ikurek0309@gmail.com>
2023-09-12 15:49:38 +0200

    ex11 done

---
+++ Iza/set2/11/reverse.cc
@@ -0,0 +1,22 @@
+# include <string>
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string inputLine;
+
+    while (getline(cin, inputLine))
+    {
+
+        for(string::const_reverse_iterator revIt = inputLine.crbegin();
+            revIt != inputLine.crend(); ++revIt)
+                cout << *revIt;
+        
+        cout << '\n';
+
+        if (inputLine.contains('-1'))
+            break;
+    }
+}
