5dc3d9fec55fbc0a97fd511a348146f257cc5811
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-18 13:50:51 +0000

    comment findNum.cc

--- Petros set2/13/findNum.cc
+++ Petros set2/13/findNum.cc
@@ -2,68 +2,64 @@
 
 using namespace std;
 
-bool isSquare(size_t);
-
 int main()
 {
-    bool state = false;
-    size_t satisfies;
+    bool state = false;                 // true is num is found
+    size_t satisfies;                   // num that satisfies req (if exists)
+
     for (size_t idx = 100; idx != 1000; ++idx)
     {   
-        if (isSquare(idx-13) && isSquare(idx+32))
+        int num1 = idx - 13
+        int num2 = idx + 32
+        bool square1 = false;           // True if num1 is square
+        bool square2 = false;           // True if num2 is square
+
+                                        // Check if num1 is square
+                                        // we start at num % 2 and
+                                        // increment by 2 because
+                                        // if num1 even/odd then
+                                        // sqrt(num1) (if integer) even/odd 
+                                        // respectively. So we only
+                                        // check odd numbers for num odd
+                                        // and vice versa
+        for (size_t idx1 = (num1 % 2); idx1 != num1; idx1 += 2)
         {
-           state = true;
-           satisfies = idx;
-           break;
+            if (num1 == idx1 * idx1)    // if so then num1 is square
+            {
+                square1 = true;         // change square state
+                break;                  // no need to continue loop
+            }
+        }
+
+
+
+                                        // Check if num2 is square
+                                        // exactly the same way as before
+        for (size_t idx1 = (num2 % 2); idx1 != num2; idx1 += 2)
+        {
+            if (num2 == idx1 * idx1)
+            {
+                square2 = true;
+                break;
+            }
+        }
+
+
+        if (square1 && square2)         // if both are square
+        {
+            state = true;               // we found our magic number
+            satisfies = idx;            // fill in its value
+            break;                      // break the loop
         }
     }
 
-    if (state)
+    if (state)                          // if we found such number
     {
         cout  << satisfies << " satisfies the requirements\n";
     }
-    else
+    else                                // if we didn't
     {
         cout << "No number satisfies the requirements";
     }
 }
-
-bool isSquare(size_t test)
-{   
-    bool state = false;
-    size_t stop = test/2;
-
-    if (test == 0 or test == 1)
-    {
-        state = true;
-    }
-    else
-    {
-        if (test % 2)
-        {
-            for (size_t idx = 3; idx < stop; idx += 2)
-            {
-                if (test == idx * idx)
-                {
-                    state = true;
-                    break;
-                }
-            }
-        }
-
-        else
-        {
-            for (size_t idx = 2; idx < stop; idx += 2)
-            {
-                if (test == idx * idx)
-                {
-                    state = true;
-                    break;
-                }
-            }
-        }
-    }
-    return state;
-}
-
 
