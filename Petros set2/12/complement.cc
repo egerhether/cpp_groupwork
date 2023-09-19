6494a6cdba6ae45b788206473b8f991c71ac0f51
pe-chrg <130558096+pe-chrg@users.noreply.github.com>
2023-09-19 17:45:30 +0100

    fixed bug on 12 negatives gave wrong out, 9,10 done

--- Petros set2/12/complement.cc
+++ Petros set2/12/complement.cc
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,11 +9,15 @@ int main(int, char *argv[])
     cout << inputNum;
 
     string binary;
-    while (inputNum != 0)
-    {   string bit = to_string(inputNum & 1);
-        binary.insert(0, bit);
-        inputNum >>= 1;
+    for (size_t idx = 0; idx != 32; ++idx)
+    {   char const bit = static_cast<char>(inputNum & 1);
+        binary.insert(0, 1,  bit + '0');
+        if (inputNum!= 0)
+            inputNum >>= 1;
+        else 
+            inputNum = 0;
     }
+
     cout << '=' << binary << '=';
 
     size_t power = 1;
@@ -22,10 +26,15 @@ int main(int, char *argv[])
     for (size_t idx = binary.length(); idx != 0; idx--)
     {
         if (binary[idx-1] == '1')
-        {
+        {   
             if (addPlus)
-                out.insert(0, "+");
+                out.insert(0, 1, '+');
+           
             out.insert(0, to_string(power));
+           
+            if (idx == 1)
+                out.insert(0, 1, '-');
+           
             addPlus = true;
         }
         power <<= 1;
