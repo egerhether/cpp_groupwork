16bd788c025b7cb38a87bca6306d13ed5b523d2e
Izabela Kurek <ikurek0309@gmail.com>
2023-10-04 18:05:17 +0200

    updates to repo

--- Iza/set3/22/fibonacci/fibonacci.h
+++ Iza/set3/22/fibonacci/fibonacci.h
@@ -1,8 +1,16 @@
 #ifndef FINDCASE_H_
 #define FINDCASE_H_
+
 #include <iostream>
 
 size_t fib(size_t value);
 size_t rawfib(size_t value);
+
+namespace 
+{
+    int const MAXFIB = 90;
+    int const ENDFIB = MAXFIB + 1;
+    size_t fibval[ENDFIB] = {};
+};
 
 #endif
