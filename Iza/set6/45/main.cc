8639de9da1e75c39de0aa03e5b3d38638c06b411
Izabela Kurek <ikurek0309@gmail.com>
2023-10-16 09:33:36 +0200

    ex 36 corrected

--- Iza/set6/45/main.cc
+++ Iza/set6/45/main.cc
@@ -4,7 +4,7 @@ int main()
 {
     Strings strings{ cin };
     string str = "hello";
-    strings.resize(strings.size()+3, str);
+    strings.resize(strings.size() + 3, str);
 
     for (size_t idx = 0, end = strings.size(); idx != end; ++idx)
         cout << "line " << idx + 1 << ": " << strings.at(idx) << '\n';
