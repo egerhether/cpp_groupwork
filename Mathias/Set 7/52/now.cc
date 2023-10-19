2fd8f6d77001afdd2a36b9ff28f5216c962d5f12
Mathias Randruut <mathias.randryyt@gmail.com>
2023-10-19 22:18:40 +0200

    52 doone

---
+++ Mathias/Set 7/52/now.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+                    // manipulator for the date and time
+struct manipulator {
+    friend ostream& operator<<(ostream& os, const manipulator& manip) {
+        time_t now = time(nullptr);
+        tm time = *localtime(&now);
+        return os << put_time(&time, "%c");
+    }
+};
+
+                    // create an object of the manipulator
+manipulator now;
+
+int main() {
+    cout << now << '\n';
+    return 0;
+}
