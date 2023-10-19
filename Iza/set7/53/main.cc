8ecbae1e400520889c48ea557f621e70960f0e23
Izabela Kurek <ikurek0309@gmail.com>
2023-10-19 16:00:20 +0200

    am dumb 53 correct now

--- Iza/set7/53/main.cc
+++ Iza/set7/53/main.cc
@@ -1,7 +1,18 @@
-#include "main.ih"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
     double value = 12.04;
-    format(cout,value);
+    cout << "std display      \"" << setw(15) << value << "\"\n"
+           << "left aligned     \"" << left << setw(15) << value  << "\"\n"
+           << "right aligned    \"" << right << setw(15) << value << "\"\n"
+           << "3 digits         \"" << setw(15) << fixed << setprecision(1) 
+           << value << "\"\n"
+           << "4-digit fraction \"" << setw(15) << setprecision(4) 
+           << value << "\"\n"
+           << "std display      \"" << setw(15) << setprecision(2) 
+           << value << "\"\n";
 }
